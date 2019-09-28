e9cc4efc6b921bd7ec138771b2d02741894eee82
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-09-27 21:39:09 -0500

    avoiding re rendering header

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 import { setSearchField, requestRobots } from '../actions';
+import Header from '../components/Header';
 
 const mapStateToProps = state => {
   return {
@@ -34,19 +35,19 @@ class App extends Component {
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return isPending ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={ onSearchChange }/>
-          <Scroll>
+    return (
+      <div className='tc'>
+        <Header/>
+        <SearchBox searchChange={ onSearchChange }/>
+        <Scroll>
+          { isPending ? "Loading" : 
             <ErrorBoundry>
               <CardList robots={ filteredRobots } />
             </ErrorBoundry>
-          </Scroll>
-        </div>
-      );
+          }
+        </Scroll>
+      </div>
+    );
   }
 }
 
