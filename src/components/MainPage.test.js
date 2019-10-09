94bd6b76f0437fd91f0f47a6f2ad7fbb8ace6fc1
Eder Ramirez <eder.ramirez87@gmail.com>
2019-10-09 09:06:44 -0500

    Testing connected components 1

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme'
+import React from 'react';
+import MainPage from '../components/MainPage';
+
+let wrapper;
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [],
+    searchField: '',
+    isPending: false
+  }
+
+  wrapper = shallow(<MainPage  {...mockProps}/>);
+});
+
+it("should render MainPage component", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("filters robots correctly", () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+      id: 3,
+      name: 'john',
+      email: 'john@gmail.com'
+    }],
+    searchField: 'john',
+    isPending: false
+  }
+  const wrapper2 = shallow(<MainPage { ...mockProps2}/>)
+  expect(wrapper2.instance().filterRobots()).toEqual([{
+    id: 3,
+    name: 'john',
+    email: 'john@gmail.com'
+  }]);
+  
+})
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
-import './App.css';
 import { setSearchField, requestRobots } from '../actions';
-import Header from '../components/Header';
+
+import MainPage from '../components/MainPage';
+import './App.css';
 
 const mapStateToProps = state => {
   return {
@@ -26,28 +23,8 @@ const mapDispatchToProps = dispatch => {
 
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
-    return (
-      <div className='tc'>
-        <Header/>
-        <SearchBox searchChange={ onSearchChange }/>
-        <Scroll>
-          { isPending ? "Loading" : 
-            <ErrorBoundry>
-              <CardList robots={ filteredRobots } />
-            </ErrorBoundry>
-          }
-        </Scroll>
-      </div>
-    );
+   return <MainPage {...this.props}/>
   }
 }
 
